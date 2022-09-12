b56dc60c3e89b3609383fbbcc1865df0f4bf999a
Tsubasa-Oba <tk2830717@yahoo.co.jp>
2022-09-12 22:42:06 +0900

    つぶやきアプリの最初のコミット

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0
+          maximum-scale=1.0, minimum-scale=1.0">
+    <title>{{ $title ?? 'つぶやきアプリ'}}</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    @stack('css')
+</head>
+<body class="bg-gray-50">
+    {{ $slot }}
+</body>
+</html>
